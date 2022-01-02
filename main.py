a55062cb2f79f6a27963826ac847e85732db50b7
Marco Polo Leyva <marco.leyva@rappi.com>
2022-01-02 14:51:26 -0600

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+#python
+from typing import Optional
+
+#pydantic
+from pydantic import BaseModel
+
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 
 app = FastAPI()
 
+
+#Models
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hail_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 
@@ -11,3 +30,10 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {"Hello": "World"}
+
+
+#Request and Response
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    return person
