04bb1e7667caa9bca476dc4cc36fca5518f3f3b0
Marco Polo Leyva <marco.leyva@rappi.com>
2022-02-07 21:20:39 -0600

    validation_body

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ app = FastAPI()
 
 #Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
 
 class Person(BaseModel):
     first_name: str
@@ -62,3 +66,22 @@ def show_person(
     person_id: int = Path(..., gt=0) #mayor a cero
 ):
     return {person_id: "It exists!"}
+
+
+#validations request body
+@app.put('/person/{person_id}')
+def update_person(
+    person_id: int = Path(
+        ..., 
+        title = "Person ID",
+        description ="This is the person id",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
+
+    
