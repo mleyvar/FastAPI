1c159a21e8314715dac008ee569be217be3018bf
Marco Polo Leyva <marco.leyva@rappi.com>
2022-02-07 23:35:03 -0600

    validations_models

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #python
 from typing import Optional
+from enum import Enum
+
 
 #pydantic
 from pydantic import BaseModel
-
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
@@ -15,17 +17,35 @@ app = FastAPI()
 
 #Models
 
+class HairColor(Enum):
+    white= "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+
 class Location(BaseModel):
     city: str
     state: str
     country: str
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hail_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50
+        )
+    last_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50
+        )
+    age: int = Field(
+        ..., 
+        gt=0,
+        le=115
+        )
+    hail_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 
 
@@ -84,4 +104,4 @@ def update_person(
     results.update(location.dict())
     return results
 
-    
+
